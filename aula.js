174031a55a6f83222393b1447779c30f8a68fe5e
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2022-10-04 16:54:25 -0300

    nd

---
+++ aula.js
@@ -0,0 +1,15 @@
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// camelCase
+
+let peso = 70;
+console.log(peso);
+let idade = 20;
+console.log(idade);
+let altura = 172
+console.log(altura)
+
+const number = 20;
+console.log(idade)
