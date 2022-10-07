aad580dee6abf2f9f611725cd0ae76368aa62a36
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2022-10-07 15:41:17 -0300

    aulas

--- aula.js
+++ aula.js
@@ -85,3 +85,8 @@ function verificarVelocidade(velocidade) {
 
     }
 }
+
+    exibirTipo(5)
+    function exibirTipo(limite)
+        for (let i = 0; i<=limite; i++)
+            if (i % 2 === 0);
---
+++ curso-em-videojs/js/ex003.js
@@ -0,0 +1,7 @@
+var n1 = Number(prompt('Digite um número')) //string
+var n2 = Number(prompt('Digite outro número')) //string
+var resultado = n1 + n2
+alert('A soma dos valores é ' + resultado) //Concatenação
+// (number + number) para adição
+// (string + string) para concatenação
+
