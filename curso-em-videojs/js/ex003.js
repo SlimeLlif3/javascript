cbfaa79e1a637699fe0c2bb6d134feff42932e6e
Yan Lucas da Mata Fernandes <yanbrokner@gmail.com>
2022-10-10 21:48:08 -0300

    atualização aula de javascript

--- curso-em-videojs/js/ex003.js
+++ curso-em-videojs/js/ex003.js
@@ -1,7 +1,7 @@
 var n1 = Number(prompt('Digite um número')) //string
 var n2 = Number(prompt('Digite outro número')) //string
 var resultado = n1 + n2
-alert('A soma dos valores é ' + resultado) //Concatenação
+alert(`A soma entre ${n1} e ${n2} é igual a ${resultado}`) //Concatenação
 // (number + number) para adição
 // (string + string) para concatenação
 
