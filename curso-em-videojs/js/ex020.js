3ecc41b4895de858e1c0eb2705039d381b59b8e5
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2022-10-19 15:31:48 -0300

    Update ex020.js

--- curso-em-videojs/js/ex020.js
+++ curso-em-videojs/js/ex020.js
@@ -1,3 +1,22 @@
-var num = [5, 2, 9, 1]
+/*
+var num = [5, 2, 9, 7]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-console.log(`Nosso vetor é o ${num}`)
+var valores = [5, 10, 29, 16, 74]
+
+for (var pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} é igual a ${valores[pos]}`)
+}
+*/
+
+var valores = [10, 14 , 29 , 16, 74]
+
+valores.sort()
+for(var pos in valores){
+    console.log(`A posição ${pos} é igual a ${valores[pos]}`)
+
+}
