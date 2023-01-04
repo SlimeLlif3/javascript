921e1535d07462ec7b9bb47069a086069019e04d
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2023-01-04 14:13:43 -0300

    projeto lampada

---
+++ desafios/lampada/js/scripts.js
@@ -0,0 +1,44 @@
+// Seleção de Elementos
+const ligar = document.getElementById('turnOn')
+const desligar = document.getElementById('turnOff')
+const lampada = document.getElementById('lamp')
+
+
+
+
+
+
+// Funções
+
+function isLampBroken (){
+    return lamp.src.indexOf ('quebrada') > -1
+
+}
+
+function lampOn (){
+    if ( !isLampBroken () ){
+        lamp.src = './img/ligada.jpg'
+    }
+    
+}
+function lampOff (){
+    if ( !isLampBroken () ){
+        lamp.src = './img/desligada.jpg'
+    }
+    
+}
+function brokenLamp(){
+    lamp.src= './img/quebrada.jpg'
+}
+
+
+
+
+
+//Eventos
+
+ligar.addEventListener('click', lampOn)
+desligar.addEventListener('click',lampOff)
+lamp.addEventListener('mouseover',lampOn)
+lamp.addEventListener('mouseout',lampOff)
+lampada.addEventListener('dblclick', brokenLamp )
